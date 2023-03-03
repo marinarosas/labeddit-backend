6e483f6443c203c633f529392d8ee781a4cb0433
Mjfreedom <mbuzina@gmail.com>
2023-03-03 12:52:49 -0300

    :sparkles: feat: Criando a pasta database e seus arquivos internos, basedb, commentsdb, postdb e userdb

---
+++ src/database/CommentsDatabase.ts
@@ -0,0 +1,135 @@
+import { LikeDislikeCommentDB, CommentDB, PostWithCreatorDB, POST_LIKE } from "../types";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class CommentsDatabase extends BaseDatabase {
+
+    public static TABLE_COMMENTS_POST = "comments_post"
+    public static TABLE_POSTS = "posts"
+    public static TABLE_LIKES_DISLIKES_COMMENTS = "likes_dislikes_comments"
+    public static TABLE_POSTS_COMMENTS = "posts_comments"
+
+    public async findComment(q: string | undefined) {
+
+        let commentDB
+
+        if (q) {
+            const result = await BaseDatabase.connection(CommentsDatabase.TABLE_COMMENTS_POST).where("content", "LIKE", `%${q}%`)
+            commentDB = result
+        } else {
+            const result = await BaseDatabase.connection(CommentsDatabase.TABLE_COMMENTS_POST)
+            commentDB = result
+        }
+
+        return commentDB
+    }
+
+    public async findCommentById(id: string | undefined): Promise<CommentDB | undefined> {
+        const [commentDBExist]: CommentDB[] | undefined[] = await BaseDatabase
+            .connection(CommentsDatabase.TABLE_COMMENTS_POST)
+            .select()
+            .where({ id: id })
+        return commentDBExist
+    }
+
+    public async insertComment(newCommentDB: CommentDB): Promise<void> {
+        await BaseDatabase.connection(CommentsDatabase.TABLE_COMMENTS_POST).insert(newCommentDB)
+
+    }
+
+    public async updateCommentById(id: string, newCommentDB: CommentDB): Promise<void> {
+        await BaseDatabase
+            .connection(CommentsDatabase.TABLE_COMMENTS_POST)
+            .update(newCommentDB)
+            .where({id})
+    }
+
+    public async deleteCommentById(id: string): Promise <void> {
+
+        await BaseDatabase
+            .connection(CommentsDatabase.TABLE_COMMENTS_POST)
+            .del()
+            .where({ id })
+    }
+
+    // public getCommentWithCreator = async (): Promise <PostWithCreatorDB[]> => {
+    //     const result: PostWithCreatorDB[] = await BaseDatabase
+    //         .connection(CommentsDatabase.TABLE_COMMENTS_POST)
+    //         .select(
+    //             "posts.id",
+    //             "posts.creator_id",
+    //             "posts.content",
+    //             "posts.likes",
+    //             "posts.dislikes",
+    //             "posts.created_at",
+    //             "posts.updated_at",
+    //             "users.name AS creator_name"
+
+    //         )
+    //         .join("users", "posts.creator_id", "=", "users.id")
+
+    //     return result
+    // }
+
+    // public findCommentWithCreatorById = async (postId: string): Promise <PostWithCreatorDB | undefined> => {
+    //     const result: PostWithCreatorDB[] = await BaseDatabase
+    //         .connection(CommentsDatabase.TABLE_COMMENTS_POST)
+    //         .select(
+    //             "posts.id",
+    //             "posts.creator_id",
+    //             "posts.content",
+    //             "posts.likes",
+    //             "posts.dislikes",
+    //             "posts.created_at",
+    //             "posts.updated_at",
+    //             "users.name AS creator_name"
+
+    //         )
+    //         .join("users", "posts.creator_id", "=", "users.id")
+    //         .where("posts.id", postId)
+
+    //     return result[0]
+    // }
+
+    public likeOrDislikeComment = async (likeDislike: LikeDislikeCommentDB): Promise <void> =>{
+        await BaseDatabase
+        .connection(CommentsDatabase.TABLE_LIKES_DISLIKES_COMMENTS)
+        .insert(likeDislike)
+    }
+
+    public findLikeDislike = async (likeDislikeToFind: LikeDislikeCommentDB): Promise <POST_LIKE | null> =>{
+        const [likeDislikeDB]: LikeDislikeCommentDB[] = await BaseDatabase
+        .connection(CommentsDatabase.TABLE_LIKES_DISLIKES_COMMENTS)
+        .select()
+        .where({
+            user_id: likeDislikeToFind.user_id,
+            comment_id: likeDislikeToFind.comment_id
+        })
+
+        if(likeDislikeDB){
+            return likeDislikeDB.like === 1 ? POST_LIKE.ALREADY_LIKED : POST_LIKE.ALREADY_DISLIKED
+        }else{
+            return null
+        }
+    }    
+
+    public removeLikeDislike = async (likeDislikeDB: LikeDislikeCommentDB): Promise <void> =>{
+        await BaseDatabase
+        .connection(CommentsDatabase.TABLE_LIKES_DISLIKES_COMMENTS)
+        .delete()
+        .where({
+            user_id: likeDislikeDB.user_id,
+            comment_id: likeDislikeDB.comment_id
+        })
+    }
+
+    public updateLikeDislike = async (likeDislikeDB: LikeDislikeCommentDB): Promise <void> =>{
+        await BaseDatabase
+        .connection(CommentsDatabase.TABLE_LIKES_DISLIKES_COMMENTS)
+        .update(likeDislikeDB)
+        .where({
+            user_id: likeDislikeDB.user_id,
+            comment_id: likeDislikeDB.comment_id
+        })
+    }
+        
+}
---
+++ src/database/labeddit.sql
@@ -0,0 +1,135 @@
+-- Active: 1674516160340@@127.0.0.1@3306
+
+CREATE TABLE users(
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    nickname TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL,
+    created_at TEXT DEFAULT (DATETIME()) NOT NULL
+);
+
+SELECT * FROM users;
+
+DROP TABLE users;
+
+INSERT INTO users (id, nickname, email, password, role)
+VALUES
+    ("u001", "Marina", "marina@email.com", "M@rina123", "ADMIN"),
+    ("u002", "Alex", "alex@email.com", "@Lex1234", "NORMAL"),
+    ("u003", "Andrea", "andrea@email.com", "Andre@123", "NORMAL");
+
+CREATE TABLE posts(
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    creator_id TEXT NOT NULL,
+    content TEXT NOT NULL,
+    likes INTEGER DEFAULT(0) NOT NULL,
+    dislikes INTEGER DEFAULT(0) NOT NULL,
+    comments INTEGER DEFAULT(0) NOT NULL,
+    created_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    updated_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+SELECT * FROM posts;
+
+DROP TABLE posts;
+
+INSERT INTO posts (id, creator_id, content)
+VALUES
+    ("p001", "u001", "Bora pra praia?!"),
+    ("p002", "u002", "Cansativo o dia!"),
+    ("p003", "u003", "Trabalhando muito!");
+
+CREATE TABLE comments_post(
+    id PRIMARY KEY UNIQUE NOT NULL,
+    post_id UNIQUE NOT NULL,
+    user_id NOt NULL,
+    content TEXT NOT NULL,
+    likes INTEGER DEFAULT(0) NOT NULL,
+    dislikes INTEGER DEFAULT(0) NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+INSERT INTO comments_post (id, post_id, user_id, content)
+VALUES
+    ("c001", "p001", "u003", "Que praia? To dentro!"),
+    ("c002", "p002", "u001", "Demais migooo! Saudades!"),
+    ("c003", "p003", "u002", "Somos dois! Bora sair pra descansar a cabeça!");
+
+CREATE TABLE likes_dislikes_posts(
+    user_id TEXT NOT NULL,
+    post_id TEXT NOT NULL,
+    like INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
+);
+
+SELECT * FROM likes_dislikes;
+
+DROP TABLE likes_dislikes;
+
+INSERT INTO likes_dislikes (user_id, post_id, like)
+VALUES
+    ("u001", "p001", 397),
+    ("u001", "p002", 269),
+    ("u002", "p003", 35);
+
+CREATE TABLE posts_comments(
+    post_id TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    comment INTEGER NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+DROP TABLE posts_comments;
+
+CREATE TABLE likes_dislikes_comments(
+    user_id TEXT NOT NULL,
+    comment_id TEXT NOT NULL,
+    like INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (comment_id) REFERENCES comments_post(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
+);
+
+SELECT 
+    posts.id,
+    posts.creator_id,
+    posts.content,
+    posts.likes,
+    posts.dislikes,
+    posts.created_at,
+    posts.updated_at,
+    users.name AS creator_name
+FROM posts
+JOIN users
+ON posts.creator_id = users.id;
+
+UPDATE users
+SET password = "$2a$12$iSd0f004.jVFVrAjslARD.myipBt.4b0Sbf/8VTvZAqIYiq6GbDju"
+WHERE id = "u001";
+
+UPDATE users
+SET password = "$2a$12$ZLXrqt.6dqrzRnpba.u8zu.S97F0V8TLVLYeU1jaBIRFNr2jCdVLu"
+WHERE id = "u002";
+
